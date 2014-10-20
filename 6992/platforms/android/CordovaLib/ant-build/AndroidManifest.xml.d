45cf5b3fd97180bdf7ae229bff52e0253a5090b8
Lisa Seacat DeLuca <ldeluca@us.ibm.com>
2014-10-20 11:38:08 -0400

    added another test for 6992

---
+++ 6992/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/CordovaLib/AndroidManifest.xml \
