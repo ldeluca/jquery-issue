90c65efdb6f3256604a4f8a7366311447eabbbee
Lisa Seacat DeLuca <ldeluca@us.ibm.com>
2014-10-20 15:00:58 -0400

    added another test for 7784

---
+++ 7784/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/AndroidManifest.xml \
---
+++ 7784/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes.dex : \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/BuildConfig.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/CordovaApp.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/R$attr.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/R$drawable.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/R$string.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/R$xml.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/classes/io/cordova/hellocordova/R.class \
+/Users/ldeluca/workspaces/jquery/6992/platforms/android/ant-build/dexedLibs/classes-172b934706966bd894eda3ae96f0b4d9.jar \
